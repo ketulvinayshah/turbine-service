a4cf4195f1f0e5187b119cab8645719400792952
ketulvinayshah <ketulvinayshah@gmail.com>
2017-08-04 01:25:33 -0700

    some

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -5,13 +5,11 @@ package com;
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableTurbine
-@EnableDiscoveryClient
 @EnableHystrixDashboard
 public class Application {
 
